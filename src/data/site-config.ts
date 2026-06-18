7e2287353a2f424d551ae6d9b0312c57e4cb5e15
Tamago <filosofish.com@gmail.com>
2026-06-18 12:21:59 +0700

    Update site-config.ts

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -52,10 +52,7 @@ const siteConfig: SiteConfig = {
         }
     ],
     socialLinks: [
-        {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
-        },
+        
         {
             text: 'Instagram',
             href: 'https://instagram.com/'
